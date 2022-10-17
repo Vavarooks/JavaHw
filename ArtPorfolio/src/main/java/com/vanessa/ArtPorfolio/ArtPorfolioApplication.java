e83fc76f013ff333d039ae2ea10ea3c8fa088729
Vanessa Emerick <vavarooks123@gmail.com>
2022-10-17 08:54:23 -0700

    New project added

---
+++ ArtPorfolio/src/main/java/com/vanessa/ArtPorfolio/ArtPorfolioApplication.java
@@ -0,0 +1,13 @@
+package com.vanessa.ArtPorfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtPorfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtPorfolioApplication.class, args);
+	}
+
+}
---
+++ ArtPorfolio/src/test/java/com/vanessa/ArtPorfolio/ArtPorfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanessa.ArtPorfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtPorfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
